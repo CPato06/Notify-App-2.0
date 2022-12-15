948fc5e487a2e658fb41923cdd006b069c3b6832
Carlos Patino <guillermo.patino@uabc.edu.mx>
2022-12-14 17:37:02 -0800

    commit

--- lib/app/data/services/dialog_service.dart
+++ lib/app/data/services/dialog_service.dart
@@ -7,6 +7,7 @@ class DialogService extends GetxService {
   static DialogService get to => Get.find();
 
   void snackBar(Color color, String title, String body) {
+    var SnackDismissDirection;
     Get.snackbar(
       title,
       body,
